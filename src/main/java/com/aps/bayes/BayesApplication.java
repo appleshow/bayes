74e0f66e9cc8f8cd0a38585f39b1373fccc9aff2
Guanbang Liu <guanbang.liu@dxc.com>
2018-12-01 00:12:37 +0800

    create project

---
+++ src/main/java/com/aps/bayes/BayesApplication.java
@@ -0,0 +1,12 @@
+package com.aps.bayes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BayesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BayesApplication.class, args);
+	}
+}
